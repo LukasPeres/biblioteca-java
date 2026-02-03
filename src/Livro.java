a7e8186a0837d760456ef143034a42afd633266c
Lucas Peres <peres07lucas@gmail.com>
2026-02-03 15:17:56 -0300

    feat: implementação completa da classe Livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,3 +1,44 @@
 public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private boolean disponivel;
+    
+    //contrutor
+    public Livro(String titulo, String autor, int anoPublicacao){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.anoPublicacao=anoPublicacao;
+        this.disponivel = true;
+    }
+    //getter
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getAutor(){
+        return autor;
+    }
+    public int getAnoPublicacao(){
+        return anoPublicacao;
+    }
+    public boolean isDisponivel(){
+        return disponivel;
+    }
+    //setter(apenas o disponivel vai mudar, logo não há nececidade de ter os outros 3 setters)
+    public void setDisponivel(boolean disponivel){
+        this.disponivel = disponivel;
+    }
 
-}
+    public void exibirInfo(){
+        System.out.println("Título: "+ titulo);
+        System.out.println("Autor: "+ autor);
+        System.out.println("Ano de publicação: "+ anoPublicacao);
+        if (disponivel != true) {
+            System.out.println("Está disponível: Não");
+        }else{
+            System.out.println("Está disponível: Sim");
+        }
+
+        }
+    }
+
